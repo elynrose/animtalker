4a686fbb22ce83c36b9a8023932cb2e0f5551087
eliezer.ayertey@lcaforesthills.org <eliezer.ayertey@lcaforesthills.org>
2024-08-23 17:35:10 -0400

    GA

--- app/Models/GenerateVideo.php
+++ app/Models/GenerateVideo.php
@@ -7,6 +7,8 @@ use Illuminate\Support\Facades\Http;
 use App\Models\Clip;
 use GuzzleHttp\Client;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
 
 
 class GenerateVideo extends Model
@@ -14,10 +16,8 @@ class GenerateVideo extends Model
     use HasFactory;
     public function generateTalkingHead($imagePath, $mp3Path, $text, $clip)
     {
-      
-
         $client = new \GuzzleHttp\Client();
-
+    
         $response = $client->request('POST', 'https://api.d-id.com/talks', [
             'body' => json_encode([
                 "script" => [
@@ -43,23 +43,19 @@ class GenerateVideo extends Model
                 'authorization' => 'Basic '.env('DID_API_KEY'),
                 'content-type' => 'application/json',
             ],
-        
-    ]);
-          
+        ]);
+    
         if ($response->getStatusCode() != 200) {
             return response()->json(['error' => 'Failed to generate video'], 500);
         }
-
-        $video = json_decode($response->getBody()->getContents(), true);
-
-        $clip->video_id = $video['id'];
-
-        $clip->save();
-
-        return redirect()->route('frontend.clips.index');
-        
     
-
+        $video = json_decode($response->getBody()->getContents(), true);
+        
+        $clip->video_id = $video['id'];
+    
+        $clip->save();
+    
+        return redirect()->route('frontend.clips.index');
     }
 }
 
