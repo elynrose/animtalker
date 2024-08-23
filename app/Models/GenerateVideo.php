843864a090d0f329e096539d2c80bbba01364f26
eliezer.ayertey@lcaforesthills.org <eliezer.ayertey@lcaforesthills.org>
2024-08-23 17:17:52 -0400

    Genv

--- app/Models/GenerateVideo.php
+++ app/Models/GenerateVideo.php
@@ -19,22 +19,48 @@ class GenerateVideo extends Model
         $client = new \GuzzleHttp\Client();
 
         $response = $client->request('POST', 'https://api.d-id.com/talks', [
-          'body' => '{"script":{"type":"text","subtitles":"false","provider":{"type":"microsoft","voice_id":"en-US-JennyNeural"},"audio_url":"https://animshorts.s3.us-east-2.amazonaws.com/audio/66c8ea935a449.mp3"},"config":{"fluent":"false","pad_audio":"0.0"},"face":{"size":1024},"source_url":"https://animshorts.s3.us-east-2.amazonaws.com/28/img-gL15XuQqbAGG5XU1Qtb9clvE.png"}',
-          'headers' => [
+            'body' => json_encode([
+                "script" => [
+                    "type" => "text",
+                    "subtitles" => "false",
+                    "provider" => [
+                        "type" => "microsoft",
+                        "voice_id" => "en-US-JennyNeural"
+                    ],
+                    "audio_url" => "https://animshorts.s3.us-east-2.amazonaws.com/audio/66c8ea935a449.mp3"
+                ],
+                "config" => [
+                    "fluent" => "false",
+                    "pad_audio" => "0.0"
+                ],
+                "face" => [
+                    "size" => 1024
+                ],
+                "source_url" => "https://animshorts.s3.us-east-2.amazonaws.com/28/img-gL15XuQqbAGG5XU1Qtb9clvE.png"
+            ]),
+            'headers' => [
+                'accept' => 'application/json',
+                'authorization' => 'Basic '.env('DID_API_KEY'),
+                'content-type' => 'application/json',
+            ],
+        'headers' => [
             'accept' => 'application/json',
             'authorization' => 'Basic '.env('DID_API_KEY'),
             'content-type' => 'application/json',
-          ],
-        ]);
-
-        $data = json_decode($response->getBody()->getContents(), true);
-
-        if (isset($data['error'])) {
-            return response()->json(['error' => $data['error']], 500);
+        ]
+    ]);
+          
+        if ($response->getStatusCode() != 200) {
+            return response()->json(['error' => 'Failed to generate video'], 500);
         }
-dd($data);
-        $clip->video_id = $data['id'];
+
+        $video = json_decode($response->getBody()->getContents(), true);
+
+        $clip->video_id = $video['id'];
+
         $clip->save();
+
+        return redirect()->route('frontend.clips.index');
         
     
 
