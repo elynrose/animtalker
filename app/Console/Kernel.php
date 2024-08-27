9214bd6bc092e8acf032ef368e8a31f9cce8cb9f
eliezer.ayertey@lcaforesthills.org <eliezer.ayertey@lcaforesthills.org>
2024-08-26 23:42:48 -0400

    kk

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
          $schedule->command('app:generate-video')->everyMinute();
-         $schedule->command('app:get-video')->everyMinute();
+         $schedule->command('app:run-video')->everyMinute();
 
     }
 
