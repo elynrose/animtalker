3d9ccf125895611cf05523975e5a1b11bf1faca4
eliezer.ayertey@lcaforesthills.org <eliezer.ayertey@lcaforesthills.org>
2024-08-27 21:08:20 -0400

    removed queable

--- app/Notifications/VideoReady.php
+++ app/Notifications/VideoReady.php
@@ -6,7 +6,6 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Notification;
 use Illuminate\Notifications\Messages\MailMessage;
-use Queueable;
 
 
 
