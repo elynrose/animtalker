30ff52f6dcba32ec2f6c030fe97543647d6ff5ff
eliezer.ayertey@lcaforesthills.org <eliezer.ayertey@lcaforesthills.org>
2024-09-02 22:56:38 -0400

    welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,34 +9,28 @@
             <div class="col-md-6">
                 <h1>AnimShorts</h1>
                 <h4>Generate Short Animations in less than 5 minutes.</h4>
-                <img src="{{ asset('images/Characters.png') }}" class="img-fluid" alt="Hero Image">
+                <p><strong>In 3 simple steps:</strong> Generate Character + Add Voice = Final Output</p>
+                <p class="text-left py-5"><a class="btn btn-lg btn-danger" href="/register">Get Started</a></p>
+
             </div>
             <div class="col-md-6"></div>
          </div>
 
          <div class="row justify-content-center">
             <div class="col-md-12">
-                <h2>How it works</h2>
                 <div class="row">
-                    <div class="col-md-3 shadow">
-                        <h4>Step 1</h4>
-                        <p>Generate a character</p>
-                        <img src="{{ asset('images/step-1.png') }}" class="img-fluid" alt="Step 1">
-                    </div>
-                    <div class="col-md-3 shadow">
-                        <h4>Step 2</h4>
-                        <p>Choose a background</p>
-                        <img src="{{ asset('images/step-2.png') }}" class="img-fluid" alt="Step 2">
+                    <div class="col-md-6 embed-responsive-item">
+                       <video src="{{ asset('images/video.mp4') }}" class="img-fluid" controls></video>
                     </div>
                     <div class="col-md-6">
-                        <h4>Step 3</h4>
-                        <p>Choose a voice</p>
-                       <video src="{{ asset('images/video.mp4') }}" class="img-fluid" controls></video>
+                    <h2 class="mb-3">What is Animshorts</h2>
+                        <p>With AnimShorts, creating eye-catching talking 3d animation heads for your social media posts is a breeze. Just input your text and watch as realistic and expressive characters come to life, ready to engage your audience with up to 255 characters of witty banter. Say goodbye to boring static images and hello to dynamic and engaging content that will make your posts stand out from the crowd. Try Generate Anim Shorts today and take your social media game to the next level!</p>
                     </div>
                 </div>
             </div>
          </div>
         </div>
+
         </div>
     </div>
 </div>
