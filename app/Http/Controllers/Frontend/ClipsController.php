35e12ff32d089dedc6e3de7ed9e1564ed4bd41b3
eliezer.ayertey@lcaforesthills.org <eliezer.ayertey@lcaforesthills.org>
2024-09-12 17:55:52 -0400

    Fixed clips

--- app/Http/Controllers/Frontend/ClipsController.php
+++ app/Http/Controllers/Frontend/ClipsController.php
@@ -139,12 +139,11 @@ class ClipsController extends Controller
         return redirect()->route('frontend.clips.index');
     }
 
-
     public function savelink(Request $request)
     {
         $video_link = $request->video_path;
         $clip = Clip::find($request->clip_id);
-        $fileName = uniqid() . '.mp4';
+        $fileName = 'video/' . uniqid() . '.mp4';
         $path = Storage::disk('s3')->putFileAs('video', $video_link, $fileName);
 
         if($path){
