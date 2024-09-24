dbcb012e6f26d5ca1be5d65fd57a77f0b702c4d4
eliezer.ayertey@lcaforesthills.org <eliezer.ayertey@lcaforesthills.org>
2024-09-24 16:30:32 -0400

    paginate

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -9,11 +9,12 @@ class HomeController
     {
         $clips = Clip::with(['character', 'media'])
         ->where('status', 'completed')
+        ->where('privacy', 0)  
         ->where('saved', 1)
         ->whereHas('character', function ($query) {
         $query->where('user_id', '!=', auth()->user()->id);
         })
-        ->orderBy('id', 'desc')->get();
+        ->orderBy('id', 'desc')->paginate(9);
 
         return view('frontend.home', compact('clips'));
     }
