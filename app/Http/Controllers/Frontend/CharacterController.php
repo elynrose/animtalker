729f760b285be08ce10617da5c32f0f830828fa1
eliezer.ayertey@lcaforesthills.org <eliezer.ayertey@lcaforesthills.org>
2024-09-26 21:44:37 -0400

    g

--- app/Http/Controllers/Frontend/CharacterController.php
+++ app/Http/Controllers/Frontend/CharacterController.php
@@ -334,7 +334,7 @@ if ($credits->getUserCredits() < 1) {
         public function refine(Request $request)
         { 
             $custom_prompt = $request->input('topic');    
-            $prompt = "rewite the following as an 3D artist would do. The prompt should generate a 3D animated character that exudes the same level of sophistication and charm as those seen in modern 3D animated movies. Example 3D art style can be found here 'https://animshorts.s3.us-east-2.amazonaws.com/46/conversions/img-GuEi5JXrr8ETyKxSjQ9G8c2q-thumb.jpg'. Photo must have an aspect ratio of 16:9 and taken at an eye-level shot. Character must alway look at the camera. Here is the prompt provided by the user:".$custom_prompt;
+            $prompt = "Rewite the following as an Character animation expert would do. The prompt should generate a 3D animated character that exudes the same level of sophistication and charm as those seen in modern 3D animated movies. Example 3D art style can be found here 'https://animshorts.s3.us-east-2.amazonaws.com/46/conversions/img-GuEi5JXrr8ETyKxSjQ9G8c2q-thumb.jpg'. Photo must have an aspect ratio of 16:9 and taken at an eye-level shot. Character must alway look at the camera. Here is the prompt provided by the user:".$custom_prompt;
             $result = SendToOpenai::sendToOpenAI($prompt);
             return $result;
         }
