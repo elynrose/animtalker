3f4ff11a53bd62cc71fc779b311dad099cb27f05
eliezer.ayertey@lcaforesthills.org <eliezer.ayertey@lcaforesthills.org>
2024-09-26 22:19:27 -0400

    max tokens

--- app/Models/SendToOpenai.php
+++ app/Models/SendToOpenai.php
@@ -23,7 +23,7 @@ class SendToOpenai extends Model
                 'json' => [
                     'model' => 'gpt-3.5-turbo-instruct',
                     'prompt' => $prompt,
-                    'max_tokens' => 1000,
+                    'max_tokens' => 500,
                     'temperature' => 0.7,
                     'top_p' => 1,
                     'frequency_penalty' => 0,
