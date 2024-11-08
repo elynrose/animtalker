1d525e21a31b3525da025ee9ca1b15934b96b4c8
Ely <elynrose29@gmail.com>
2024-11-07 21:07:01 -0500

    g

--- resources/views/frontend/clips/index.blade.php
+++ resources/views/frontend/clips/index.blade.php
@@ -11,7 +11,7 @@
                 @if(!$clips->isEmpty())
                 @foreach($clips as $clip)
                 @if($clip->character)
-                    <div class="col-md-3">
+                    <div class="col-md-4">
                         <div class="card mb-4">
                             
                         @can('character_delete')
